a46f3cea818dd757a3f0e122413391245e8a13ff
Divyanshu Vats <divyanshuvats2807@gmail.com>
2026-08-23 04:21:57 +0530

    updated Java revision

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,29 @@
+package javaRevision.Arrays;
+
+public class linearSearch {
+
+    public static int linearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+               return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 3, 4, 5, 7, 9, 20, 29, 44, 55, 60 };
+        int key = 20;
+
+       int index =  linearSearch(numbers, key);
+       if(index == -1){
+        System.out.println("key is not found");
+       }
+       else{
+        System.out.println("key is found at index: " + index);
+       }
+       
+
+    }
+}
